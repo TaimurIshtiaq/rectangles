6a76b39544d47d05b8d317eb44c0f57a6d567e8b
TaimurIshtiaq <56674122+TaimurIshtiaq@users.noreply.github.com>
2019-10-20 01:40:47 +0500

    Update rectangles.py

--- rectangles.py
+++ rectangles.py
@@ -5,13 +5,14 @@ arr = [[1, 1, 1],
 def numRectangles():
     counter = 0
     pairs = []
-
+	
     for i in range(len(arr) - 1):
         for j in range(i + 1, len(arr)):
             for k in range(len(arr[i])):
                 if arr[i][k] == arr[j][k] == 1:
                     pairs.append([[i, k], [j, k]])
 
+	# For comparing elevation
     for i in range(len(pairs) - 1):
         for j in range(i + 1, len(pairs)):
             if pairs[i][0][0] == pairs[j][0][0] and pairs[i][1][0] == pairs[j][1][0]:
